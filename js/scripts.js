f267fd004155df13b1c6048822ff5f7069d10f66
Brian Scherner <brianscherner@gmail.com>
2023-05-18 09:19:44 -0700

    Make array from user selections. Loop through userSelectionsArray variable with a forEach method. Pass in the callback function to create a new paragraph element using 'paragraph' variable. Add input value from each element value via append method. Add new paragraph to the end of document via append method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 function handleForm(event) {
 	event.preventDefault();
 	const userSelections = document.querySelectorAll("input[name=transportation-option]:checked");
+	const userSelectionsArray = Array.from(userSelections);
+
+	userSelectionsArray.forEach(function(element) {
+		const paragraph = document.createElement("p");
+		paragraph.append(element.value);
+		document.body.append(paragraph);
+	});
 }
 
 window.addEventListener("load", function() {
